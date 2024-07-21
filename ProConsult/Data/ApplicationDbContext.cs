7db63af51f5d9f4fe8629ee4118eb5e683638eeb
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2024-07-21 12:01:52 +0100

    added db set mapping

--- ProConsult/Data/ApplicationDbContext.cs
+++ ProConsult/Data/ApplicationDbContext.cs
@@ -1,9 +1,25 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ProConsult.Models;
+using System.Reflection;
 
 namespace ProConsult.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+        : IdentityDbContext<ApplicationUser>(options)
     {
+        //mapping db sets to build the tables
+        public DbSet<Specialist> Specialists { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+
+        //method to initialize the configuration
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(builder);
+        }
+
     }
 }
