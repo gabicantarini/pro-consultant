23efe9dc5a56edc2fdd55a817b515ce85a636003
Gabriela Iaccovazzo de Almeida Cantarini <gabilana64@hotmail.com>
2024-07-23 22:32:57 +0100

    added index.razor

---
+++ ProConsult/Components/Pages/Patients/Index.razor.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using MudBlazor;
+using ProConsult.Models;
+using ProConsult.Repositories.Patients;
+
+namespace ProConsult.Components.Pages.Patients
+{
+    public class IndexPage : ComponentBase
+    {
+        [Inject]
+        public IPatientRepository repository { get; set; } = null!;
+
+        [Inject]
+        public IDialogService Dialog { get; set; } = null!;
+
+        [Inject]
+        public ISnackbar Snackbar { get; set; } = null!;
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; } = null!;
+
+        public List<Patient> Patients { get; set; } = new List<Patient>();
+
+        [CascadingParameter]
+        private Task<AuthenticationState> AuthenticationState { get; set; }
+
+        public bool HideButtons { get; set; }
+
+        public async Task DeletePatient(Patient patient)
+        {
+            try
+            {
+                var result = await Dialog.ShowMessageBox
+                (
+                    "Atenção",
+                    $"Deseja excluir o paciente {patient.Name}?",
+                    yesText: "SIM",
+                    cancelText: "NÃO"
+                );
+
+                if (result is true)
+                {
+                    await repository.DeleteByIdAsync(patient.Id);
+                    Snackbar.Add($"Patient {patient.Name} successfully removed!", Severity.Success);
+                    await OnInitializedAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                Snackbar.Add(ex.Message, Severity.Error);
+            }
+        }
+
+        public void GoToUpdate(int id)
+        {
+            NavigationManager.NavigateTo($"/pacientes/update/{id}");
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var auth = await AuthenticationState;
+
+            HideButtons = !auth.User.IsInRole("Attendant");
+
+            Patients = await repository.GetAllAsync();
+        }
+    }
+}
--- ProConsult/obj/Debug/net8.0/ProConsult.AssemblyInfo.cs
+++ ProConsult/obj/Debug/net8.0/ProConsult.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProConsult")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5271e8b63fddd48d302d8176393bbaeef1496521")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58c15777767c17474b7aa604a8080ab2d2a436b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProConsult")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProConsult")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
